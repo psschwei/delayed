1a694bafed4b9e88fb34409dc215371e960a61cf
Paul S. Schweigert <paul@paulschweigert.com>
2022-09-16 09:32:31 -0400

    update config, add request processing delay
    Signed-off-by: Paul S. Schweigert <paul@paulschweigert.com>

--- main.go
+++ main.go
@@ -10,29 +10,35 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	target := "delayed"
-	fmt.Fprintf(w, "Hello %s!\n", target)
+	fmt.Fprintf(w, "Starting to handle request\n")
+	doDelay("REQUEST")
+	fmt.Fprintf(w, "Request handled\n")
+}
+
+// doDelay takes the name of an environmental variable and sleeps that amount of time
+func doDelay(envvar string) {
+	delay, err := strconv.Atoi(os.Getenv(envvar))
+	if err != nil {
+		delay = 0
+	}
+	if delay > 0 {
+		for i := 0; i < delay; i++ {
+			time.Sleep(1 * time.Second)
+		}
+	}
 }
 
 func main() {
-	log.Print("delayed: simulating slow startup...")
+	log.Print("starting server")
+	doDelay("STARTUP")
+	log.Print("server ready")
 
-	delay, err := strconv.Atoi(os.Getenv("SLEEP"))
-	if err != nil {
-		delay = 30
-	}
-	for i := 0; i < delay; i++ {
-		time.Sleep(1 * time.Second)
-	}
-
-	log.Print("delayed: starting server...")
 	http.HandleFunc("/", handler)
-
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
 
-	log.Printf("delayed: listening on port %s", port)
+	log.Printf("listening on port %s", port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
 }
